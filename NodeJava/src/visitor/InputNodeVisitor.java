9c969609940206a366d7ca8aa94828be15472952
jelmer van der schoot <jschoot2@student.avans.nl>
2016-06-24 12:52:54 +0200

    Oeps vergeten

--- NodeJava/src/visitor/InputNodeVisitor.java
+++ NodeJava/src/visitor/InputNodeVisitor.java
@@ -1,3 +1,12 @@
 package visitor;
 
+import controller.NodeHolder;
+import nodes.InputHighNode;
+import nodes.InputLowNode;
+
+public interface InputNodeVisitor {
+	
+	public void visit(InputLowNode low);
+	public void visit(InputHighNode high);
+
 }
